f1045693747361da13aa3bf6c295202719ef90bf
Aaron Bassett <me@aaronbassett.com>
2015-03-08 17:46:37 +0000

    Add more styles and iconic

--- src/coffee/app.coffee
+++ src/coffee/app.coffee
@@ -7,9 +7,9 @@ class HeaderText
   backDelay: 5000
   loop: true
   texts: [
-    "a better web"
-    "reliable applications"
-    "future technologies"
+    "a better web."
+    "reliable applications."
+    "future technologies."
   ]
 
   constructor: ->
@@ -24,21 +24,62 @@ class HeaderText
 class Quotes
 
   div: $("#❤")
+  wrapper: $(".quotes-wrapper")
 
   constructor: ->
     @blocks = @div.find "blockquote"
     @max = @blocks.length - 1
     @index = Math.floor(Math.random() * @max)
-    console.log @index
+
+    @t = new Trianglify
+      x_gradient: Trianglify.colorbrewer.YlOrRd[9]
+      y_gradient: Trianglify.colorbrewer.PuBuGn[9]
+      cellsize: 50
+    @pattern = @t.generate document.body.clientWidth, 300
 
   go: ->
-    $(@blocks[@index]).hide 500, =>
+    $(@blocks[@index]).animate
+      opacity: 0
+    , 2000, =>
+      do $(@blocks[@index]).hide
       @index = if @index == @max then 0 else @index + 1
-      $(@blocks[@index]).show 500
+      $(@blocks[@index]).show().animate
+        opacity: 1
+      , 500
+
+  renderpolys: ->
+    @wrapper.css 'background-image', @pattern.dataUrl
 
 
+
+
+class LowPolygonHeader
+
+  header: $(".lowpoly")
+
+  constructor: ->
+    @t = new Trianglify
+      x_gradient: Trianglify.colorbrewer.RdPu[4]
+      y_gradient: Trianglify.colorbrewer.YlOrRd[9]
+      cellsize: 150
+    @pattern = @t.generate document.body.clientWidth, 350
+
+  render: ->
+    @header.css 'background-image', @pattern.dataUrl
+
+  text: ->
+    $(".lowpoly h1").fitText 1
+
+
+# Page header
 typing = new HeaderText
+lowpoly = new LowPolygonHeader
+do lowpoly.render
+do lowpoly.text
+
+# Quotes from some lovely people
 lovelyPeople = new Quotes
+do lovelyPeople.renderpolys
 do lovelyPeople.go
 
 setInterval ->
--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 (function() {
-  var HeaderText, Quotes, lovelyPeople, typing;
+  var HeaderText, LowPolygonHeader, Quotes, lovelyPeople, lowpoly, typing;
 
   HeaderText = (function() {
     HeaderText.prototype.span = $("#✓");
@@ -12,7 +12,7 @@
 
     HeaderText.prototype.loop = true;
 
-    HeaderText.prototype.texts = ["a better web", "reliable applications", "future technologies"];
+    HeaderText.prototype.texts = ["a better web.", "reliable applications.", "future technologies."];
 
     function HeaderText() {
       this.span.typed({
@@ -31,29 +31,77 @@
   Quotes = (function() {
     Quotes.prototype.div = $("#❤");
 
+    Quotes.prototype.wrapper = $(".quotes-wrapper");
+
     function Quotes() {
       this.blocks = this.div.find("blockquote");
       this.max = this.blocks.length - 1;
       this.index = Math.floor(Math.random() * this.max);
-      console.log(this.index);
+      this.t = new Trianglify({
+        x_gradient: Trianglify.colorbrewer.YlOrRd[9],
+        y_gradient: Trianglify.colorbrewer.PuBuGn[9],
+        cellsize: 50
+      });
+      this.pattern = this.t.generate(document.body.clientWidth, 300);
     }
 
     Quotes.prototype.go = function() {
-      return $(this.blocks[this.index]).hide(500, (function(_this) {
+      return $(this.blocks[this.index]).animate({
+        opacity: 0
+      }, 2000, (function(_this) {
         return function() {
+          $(_this.blocks[_this.index]).hide();
           _this.index = _this.index === _this.max ? 0 : _this.index + 1;
-          return $(_this.blocks[_this.index]).show(500);
+          return $(_this.blocks[_this.index]).show().animate({
+            opacity: 1
+          }, 500);
         };
       })(this));
+    };
+
+    Quotes.prototype.renderpolys = function() {
+      return this.wrapper.css('background-image', this.pattern.dataUrl);
     };
 
     return Quotes;
 
   })();
 
+  LowPolygonHeader = (function() {
+    LowPolygonHeader.prototype.header = $(".lowpoly");
+
+    function LowPolygonHeader() {
+      this.t = new Trianglify({
+        x_gradient: Trianglify.colorbrewer.RdPu[4],
+        y_gradient: Trianglify.colorbrewer.YlOrRd[9],
+        cellsize: 150
+      });
+      this.pattern = this.t.generate(document.body.clientWidth, 350);
+    }
+
+    LowPolygonHeader.prototype.render = function() {
+      return this.header.css('background-image', this.pattern.dataUrl);
+    };
+
+    LowPolygonHeader.prototype.text = function() {
+      return $(".lowpoly h1").fitText(1);
+    };
+
+    return LowPolygonHeader;
+
+  })();
+
   typing = new HeaderText;
 
+  lowpoly = new LowPolygonHeader;
+
+  lowpoly.render();
+
+  lowpoly.text();
+
   lovelyPeople = new Quotes;
+
+  lovelyPeople.renderpolys();
 
   lovelyPeople.go();
 
